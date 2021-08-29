86a5a994cbd9734dfe303eea6094e083363878d0
aviradinsky <avi.radinsky@gmail.com>
2021-08-29 15:08:57 -0400

    initial commit

---
+++ partition.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def partition(x: int):
+    """
+    Returns the partition of integer 'x'
+    """
+    return sum([partition_x_base_n(x,i+1) for i in range(x)])
+
+@lru_cache(maxsize=None)
+def partition_x_base_n(x: int,n: int):
+    """
+    Returns the number of ways to partition 'x' into 'n' integers greater than 0
+    """
+    if x == n: return 1
+    if x-n <= n: return partition(x-n)
+    return sum([partition_x_base_n(x-n,i+1) for i in range(n)])
